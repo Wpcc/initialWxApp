7f6e3342799db225f5a7a1721b15529d08dc2586
yijiangchunshui <787376932@qq.com>
2019-03-16 16:18:32 +0800

    change map

--- 测试/location/pages/index/index.js
+++ 测试/location/pages/index/index.js
@@ -5,36 +5,29 @@ const app = getApp()
 Page({
   data:{
     getLocation: false,
-    latitude: 23.099994,
-    longitude: 113.324520,
-    compassTrue: true,
+    latitude: 30.565543,
+    longitude: 114.206138,
+    messageShow: true,
     markers: [
       {
         id: 0,
-        latitude: 23.099994,
-        longitude: 113.324520
-      },
-      {
-        id: 1,
-        latitude: 56.099994,
-        longitude: 113.324520
-      },
-      {
-        id: 2,
-        latitude: 23.099994,
-        longitude: 100.324520
+        latitude: 30.565543,
+        longitude: 114.206138,
+        iconPath: '../../static/images/location_no.png',
+        width:40,
+        height:40
       }
     ],
   },
   onShow: function () {
-    const that = this
+    let that = this
     wx.getLocation({
-      type: 'wgs84',
+      type: 'gcj02',
       success(res) {
         that.setData({
           getLocation: true,
-          'latitude':res.latitude,
-          'longitude':res.longitude
+          latitude:res.latitude,
+          longitude:res.longitude
         })
         console.log(res.latitude)
         console.log(res.longitude)
@@ -56,5 +49,22 @@ Page({
     }else{
       console.log('你已经授权成功了')
     }
+  },
+  callout: function (e) {
+    console.log('clicked marker')
+    console.log(e.markerId)
+    this.setData({
+      'markers[0].width':60,
+      'markers[0].height':60
+    })
+  },
+  goMap: function () {
+    wx.openLocation({
+      latitude:this.data.latitude,
+      longitude:this.data.longitude,
+      name:'汉西一路',
+      address:'汉西一路古五社区',
+      scale:18
+    })
   }
 })
--- 电宝宝充电桩/readme.md
+++ 电宝宝充电桩/readme.md
@@ -72,15 +72,59 @@ icon图标选择：多色图标库-智能城市
 
 ### **首页**
 
-**map：**
+#### map
 
 以用户坐标轴为中心，显示附近充电桩位置。在地图上可添加扫码图标，点击调取微信扫码功能，扫码成功进入支付详情页。
 
-**搜索框：**
+其次，点击附件充电桩图标，充电桩图标放大，底部出现充电桩位置详情页面，点击跳转到微信内置导航，显示用户位置和当前充电桩位置，底部出现导航地图选择，包括（腾讯，百度，等）
+
+**中心点坐标误差：**
+
+当使用`wx.getLocation`获取用户当前的地理位置，并将该地理位置赋值给中心点（longitude，latitude）的时候，中心点位置和用户当前位置存在巨大的偏移误差。
+
+代码如下：
+
+```javascript
+onShow:function () {
+    const that = this
+    wx.getLocation({
+        type:'wgs84',
+        success (res) {
+            that.setData({
+                getLocation:true,
+                latitude:res.latitude,
+                longitude:res.longitude
+            })
+        }
+    })
+}
+//wgs84返回的是gps坐标，gcj02返回的是火星坐标可用于wx.openLocation的坐标
+```
+
+将wgs84调整成gcj02便能减少中心点与用户当前位置的偏差。
+
+**获取marker点的ID**
+
+```javascript
+callout: function(e) { //markers绑定函数
+    console.log('clicked marker')
+    console.log(e.markerId)
+}
+```
+
+**覆盖在map组件上的弹出框：**
+
+API:
+
+​	controls(控件，即将废除，建议使用cover-view代替)，需要注意地是cover-view需要在需要覆盖的组件（如：map）之上。
+
+
+
+#### 输入框
 
 这个逻辑处理比较麻烦，点击搜索框进入到列表页面，列表页面通过`localstorage`存储用户搜索记录，同时显示附近的充电桩，如果为详细的充电桩，则跳转到支付详情页。如果为详细地址，则回跳到主页，并改变map中的数据。
 
-**我要充电：**
+#### 我要充电
 
 跳转地址和搜索框地址一样，跳转到list页面。
 
